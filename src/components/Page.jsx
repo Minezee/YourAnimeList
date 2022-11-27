e16719580fd3bb87f22dacaba0564d3c99bb14b9
Nauval Fahreza A <nauvalfahreza03@gmail.com>
2022-11-27 22:59:37 +0700

    fix API bug on sorting

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,12 +1,16 @@
 import { Error, Card, PageLoad } from "../components";
 
 import { RiArrowLeftSLine, RiArrowRightSLine } from 'react-icons/ri'
+import { useSearchParams } from "react-router-dom";
 
 
 const Page = ({ data, isFetching, setPage, page }) => {
+    const [searchParams] = useSearchParams({ q: ' ' });
+    const query = searchParams.get('q');
     const hasNextPage = data?.pagination?.has_next_page;
     const totalPage = data?.pagination?.last_visible_page;
     const pageArr = []
+    const dataList = data?.data;
 
     if (page < 4) {
         if(totalPage < 7){
@@ -37,7 +41,7 @@ const Page = ({ data, isFetching, setPage, page }) => {
                 {isFetching ?
                     <PageLoad />
                     :
-                    data?.data?.map((anime) => (
+                    dataList?.filter(name => name.title.toLowerCase().includes(query.toLowerCase())).map((anime) => (
                         <div
                             key={anime.mal_id}
                             className="min-w-[20%] max-w-[20%] md:max-w-[15%] md:min-w-[15%] mx-2 mb-[10px] md:mx-6 md:mb-6]">
